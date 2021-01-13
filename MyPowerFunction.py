c1bb5d0c9c193eb181f1d6a4f1177e9afe6c1ee9
Elius <graffe1@spu.edu>
2021-01-12 16:34:05 -0800

    running  code and including data

--- MyPowerFunction.py
+++ MyPowerFunction.py
@@ -1,5 +1,6 @@
 #MyPowerFunction.py
 import csv
+import sys
 from time import perf_counter_ns
 
 def RecursiveFunction(base, exponent):
@@ -10,31 +11,31 @@ def RecursiveFunction(base, exponent):
 
 base = 3.14159265359
 exponent = 1
-total1 = 1
-total2 = 1
+total = 1
+
+sys.setrecursionlimit(1000000)
 
 with open('datafile.csv', 'w', newline='') as CSVfile:
     writeToMe = csv.writer(CSVfile)
     writeToMe.writerow(["exponent", "recursive time", "iterative time"])
+
     while True :
-        iterativeTime = perf_counter_ns()
+        
         #start timer1
-        
+        iterativeTime = perf_counter_ns()
         for i in range(1, exponent+1):
-            total1 *= base
-        #end timer1
+            total *= base
         iterativeTime = perf_counter_ns() - iterativeTime
-
+        #end timer1
         
-
-        recursiveTime = perf_counter_ns()
         #start timer2
-        total2 = RecursiveFunction(base, exponent)
-        #stop timer2
+        recursiveTime = perf_counter_ns()
+        RecursiveFunction(base, exponent)
         recursiveTime = perf_counter_ns() - recursiveTime
-
-        writeToMe.writerow([ exponent, recursiveTime, iterativeTime, total1, total2])
-        total1 = 1
+        #stop timer2
+        
+        writeToMe.writerow([ exponent, recursiveTime, iterativeTime])
+        total = 1
 
         #write times and numbers to CSV file
         exponent += 1
